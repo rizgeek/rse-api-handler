d806841d778063bf564132da3769c5ed8f8532ea
Muhammad Rizki <rizgeek@gmail.com>
2025-10-27 13:13:52 +0700

    chore : set bind port and host from env

--- src/routes/server.rs
+++ src/routes/server.rs
@@ -1,11 +1,37 @@
+use std::env;
+
+use dotenv::dotenv;
 use tokio::{net::TcpListener, signal};
 
 use crate::routes::routes::routes;
 
+struct ApiConfig {
+    port: String,
+    host: String
+}
+
+impl ApiConfig {
+    fn new(port: String, host: String) -> Self {
+        Self { port, host }
+    }
+
+    fn bind(self) -> String  {
+        format!("{}:{}",self.host, self.port)
+    }
+}
+
+
 pub async fn server() {
+    dotenv().ok();
+
     let app = routes().await;
 
-    let listener = TcpListener::bind("127.0.0.1:4444")
+    let port = env::var("PORT").expect("PORT must be set in .env");
+    let host = env::var("HOST").expect("HOST must be set in .env");
+    
+    let bind = ApiConfig::new(port, host).bind();
+
+    let listener = TcpListener::bind(bind)
         .await
         .expect("Failed to bind address");
 
