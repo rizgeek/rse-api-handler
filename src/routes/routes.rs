75a651cfbab45982d1496d392d0366a01a2b312f
Muhammad Rizki <rizgeek@gmail.com>
2025-10-27 13:46:31 +0700

    feat : nesting structure

--- src/routes/routes.rs
+++ src/routes/routes.rs
@@ -2,9 +2,18 @@ use axum::{routing::get, Router};
 
 use crate::handler::global::handler_hello_world;
 
-pub async fn routes() -> Router {
+async fn global() -> Router {
     let app: Router = Router::new()
-        .route("/", get(handler_hello_world));
+        .route("/hello", get(handler_hello_world));
+
+    app
+}
+
+pub async fn routes() -> Router {
+    let global = global().await;
+
+    let app: Router = Router::new()
+        .nest("/global", global);
 
     app
 }
