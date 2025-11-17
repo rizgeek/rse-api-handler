007b515178a1aa88a3bbf1be1cc385fe9af579d9
Muhammad Rizki <rizgeek@gmail.com>
2025-11-18 05:56:54 +0700

    learn : hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+// use std::net::SocketAddr;
+
+
+// use axum::{routing::get, Router};
+
+use std::{thread, time::Duration};
+
+
+
+async fn handler_hello()  {
+    thread::sleep(Duration::from_secs(5));
+    print!("Hello MF");
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let app: Router = Router::new()
+    //     .route("/", get(handler_hello));
+
+    // let addr = SocketAddr::from(([0,0,0,0], 3000));
+
+    // let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    // axum::serve(listener,app)
+    //     .await
+    //     .unwrap()
+    handler_hello().await;
+    println!("Hello world");
+
 }
